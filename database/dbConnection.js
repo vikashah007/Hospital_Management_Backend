e7cd0d96f39d9d5ea4757498f6069ad38089eb97
Vikash <itscoolvikash@gmail.com>
2024-04-28 14:47:34 +0530

    resolve db connectivity

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const dbConnection=()=>{
+
     mongoose.connect(process.env.MONGO_URI,{
-        dbName:"hospital"
     })
     .then(() => {
         console.log('Connected to MongoDB');
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ cloudinary.v2.config({
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET
 })
+
+
 app.listen(4000,()=>{
     console.log(`server is listening at ${process.env.PORT} port`)
 })
